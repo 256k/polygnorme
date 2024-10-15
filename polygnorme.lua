77a40e7ab216c16b96c8d5664986ee6e0e3f3192
Fayek Helmi <fayek.helmi@gmail.com>
2024-10-15 17:41:49 -0400

    Update polygnorme.lua to last changes in branch post git corruption

--- polygnorme.lua
+++ polygnorme.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: lowercase-global
 -- polygnomre
 -- a soft recreation of polygome from stretta
 -- based on a small demo video on YT.
@@ -27,6 +28,7 @@
 -- perhaps have a reverse and random mode of play
 --
 
+
 engine.name = "PolyPerc"
 local g = grid.connect()
 local mu = require "musicutil"
@@ -37,100 +39,157 @@ local g_rows = g.device.rows
 -- ===================
 local root_num = 24
 local scale = "Major"
-local gen_scale = mu.generate_scale_of_length(root_num, scale, g_cols)
+local gen_scale = mu.generate_scale_of_length(root_num, scale, g_cols) or {}
 tab.print(gen_scale)
 local g_scale_map = {}
-local MODE = "record"
-local MODE_LIST = {"play", "record"}
-
+local MODE = "direct"
+local MODE_LIST = { "direct", "play", "record" }
+local REC_NOTE_SEQ = {}
+local seq_clock
 -- init scale map
 -- ===============
 function init_scale_map()
-    for y = 1, g_rows do
-        g_scale_map[y] = {}
-        for x = 1, g_cols do
-          local note_offset = gen_scale[(17-y)]
-            g_scale_map[y][x] = { 
-              x = x, 
-              y = y, 
-              note = (gen_scale[x] + note_offset)
-              
-            }
-        end
+  for y = 1, g_rows do
+    g_scale_map[y] = {}
+    for x = 1, g_cols do
+      local note_offset = gen_scale[(17 - y)]
+      g_scale_map[y][x] = {
+        x = x,
+        y = y,
+        note = gen_scale[x] + note_offset
+
+      }
     end
+  end
 end
 
 -- init params options:
 -- ====================
 function init_params()
   scale_names = {}
-for i = 1, #mu.SCALES do
-  table.insert(scale_names, mu.SCALES[i].name)
-end
+  for i = 1, #mu.SCALES do
+    table.insert(scale_names, mu.SCALES[i].name)
+  end
 
-    params:add{
-            type = "number",
-            id = "root_note",
-            name = "root note",
-            min = 0,
-            max = 127,
-            default = root_num,
-            formatter = function(param)
-                return mu.note_num_to_name(param:get() * 2, true)
-            end,
-            action=function(x)
-              root_num = x
-              update_scale_map()
-            end
-        }
-  params:add{
-            type = "option",
-            id = "scale",
-            name = "scale",
-            options = scale_names,
-            default = 1,
-            action=function(x)
-              scale = scale_names[x]
-              update_scale_map()
-            end
-        }
-        
-  params:add{
-            type = "option",
-            id = "MODE",
-            name = "mode",
-            options = MODE_LIST,
-            default = 1,
-            action=function(x)
-              MODE = MODE_LIST[x]
-            end
-        }
-    
+  params:add {
+    type = "number",
+    id = "root_note",
+    name = "root note",
+    min = 0,
+    max = 127,
+    default = root_num,
+    formatter = function(param)
+      return mu.note_num_to_name(param:get() * 2, true)
+    end,
+    action = function(x)
+      root_num = x
+      update_scale_map()
+    end
+  }
+  params:add {
+    type = "option",
+    id = "scale",
+    name = "scale",
+    options = scale_names,
+    default = 1,
+    action = function(x)
+      scale = scale_names[x]
+      update_scale_map()
+    end
+  }
+
+  params:add {
+    type = "option",
+    id = "mode",
+    name = "Mode",
+    options = MODE_LIST,
+    default = 1,
+    action = function(x)
+      MODE = MODE_LIST[x]
+    end
+  }
 end
 
 function update_scale_map()
-    gen_scale = mu.generate_scale_of_length(root_num, scale, g_cols)
-    init_scale_map()
+  gen_scale = mu.generate_scale_of_length(root_num, scale, g_cols) or {}
+  init_scale_map()
 end
 
-
 function init()
-    init_params()
-    init_scale_map()
-    tab.print(g_scale_map[16][1])
+  init_params()
+  init_scale_map()
+
+  tab.print(g_scale_map[16][1])
 end
 
 function g.key(x, y, z)
-    if z == 1 and g_scale_map[y][x].note < 127 then
-        print("note", g_scale_map[y][x].note, mu.note_num_to_name(g_scale_map[y][x].note))
-        
-        engine.hz(mu.note_num_to_freq(g_scale_map[y][x].note))
-    end
-    draw_grid_note(x,y,z)
+  -- determine pressed note:
+  local pressed_note
+  if z == 1 then
+    pressed_note = g_scale_map[y][x]
+  end
+  -- =======================
+
+
+
+  -- play mode:
+  if MODE == 'play' and z == 1 then
+    seq_clock = clock.run(function() 
+      play_seq(pressed_note) 
+      draw_grid_note(pressed_note.x, pressed_note.y, 15)
+      end)
+    return
+  elseif MODE == 'play' and z == 0 then
+    clock.cancel(seq_clock)
+  end
+
+  -- =============
+  -- Record Mode:
+  if MODE == 'record' and z == 1 then
+    table.insert(REC_NOTE_SEQ, pressed_note)
+    tab.print(REC_NOTE_SEQ)
+  end
+  -- ==============
+
+  if z == 1 and g_scale_map[y][x].note < 127 then
+    engine.hz(mu.note_num_to_freq(pressed_note.note))
+  end
+  draw_grid_note(x, y, z)
 end
 
-function draw_grid_note(x,y,z)
-    g:led(g_scale_map[y][x].x,g_scale_map[y][x].y, z*15)
+function draw_grid_note(x, y, z)
+  g:led(g_scale_map[y][x].x, g_scale_map[y][x].y, z * 15)
+  g:refresh()
+end
+
+function key(n, z)
+  if n == 3 and z == 1 then params:set('mode', 3) end
+  if n == 3 and z == 0 then params:set('mode', 2) end
+end
+
+function test()
+  while true do
+    clock.sync(1 / 4)
+    engine.hz(220)
+  end
+end
+
+function play_seq(played_note)
+  local i = 1
+  while true do
+    clock.sync(1 / 4)
+    print("note played: ", played_note.note)
+    if i > #REC_NOTE_SEQ then i = 1 end
+    print("i: ", i)
+
+    -- REC_NOTE_SEQ[i].note - REC_NOTE_SEQ[1].note is so we can get the relative note intervals starting with 0 at the first note
+    local resulting_note = played_note.note + (REC_NOTE_SEQ[i].note - REC_NOTE_SEQ[1].note)
+    print("resulting_note: ", resulting_note)
+    engine.hz(mu.note_num_to_freq(resulting_note))
+    if i > 1 then g:led(REC_NOTE_SEQ[i - 1].x, REC_NOTE_SEQ[i - 1].y, 0 ) end
+    g:led(REC_NOTE_SEQ[i].x, REC_NOTE_SEQ[i].y, 15 )
     g:refresh()
+    i = i + 1
+  end
 end
 
